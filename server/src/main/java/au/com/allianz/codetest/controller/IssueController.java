83a0ca0fbf27a5c9b80fc788f45aeee73d29b844
Arthur Clement <arthur.clement@allianz.com.au>
2015-04-24 13:35:46 +1000

    issue controller and service server side

--- server/src/main/java/au/com/allianz/codetest/controller/IssueController.java
+++ server/src/main/java/au/com/allianz/codetest/controller/IssueController.java
@@ -5,72 +5,66 @@
 package au.com.allianz.codetest.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import au.com.allianz.codetest.model.Issue;
+import au.com.allianz.codetest.service.IssueService;
 
-@Controller
-@RequestMapping("/rest")
+
+@RestController
+@RequestMapping("/issue")
 public class IssueController {
   private static final Logger logger = LoggerFactory.getLogger(IssueController.class);
   
+  @Autowired
+  private IssueService issueService;
   
-  private Map<Integer, Issue> issueDatastore = new HashMap<Integer, Issue>();
-   
-  @RequestMapping(value = "/issue/dummy", method = RequestMethod.GET)
-  public @ResponseBody Issue getDummyIssue() {
+  @RequestMapping(value = "/dummy", method = RequestMethod.GET)
+  public Issue getDummyIssue() {
       logger.info("Start getDummyIssue");
+      Double id = Math.random() * 100;
       Issue issue = new Issue();
-      issue.setId(9999);
-      issue.setName("DummyIssue");
+      issue.setId(id.intValue());
+      issue.setDescription("DummyIssue");
       issue.setCreatedDate(new Date());
-      issueDatastore.put(9999, issue);
+      
+      issueService.addIssue(issue);
+      
       return issue;
   }
    
-  @RequestMapping(value = "/issue/{id}", method = RequestMethod.GET)
-  public @ResponseBody Issue getIsssue(@PathVariable("id") int id) {
-      logger.info("Start getIsssue. ID=" + id);       
-      return issueDatastore.get(id);
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public Collection<Issue> getIssues() {
+      logger.info("get issues");
+      return issueService.getIssues();
   }
    
-  @RequestMapping(value = "/issues", method = RequestMethod.GET)
-  public @ResponseBody List<Issue> getAllIssue() {
-      logger.info("Start getAllIssue.");
-      List<Issue> issues = new ArrayList<Issue>();
-      Set<Integer> keys = issueDatastore.keySet();
-      for(Integer i : keys){
-        issues.add(issueDatastore.get(i));
-      }
-      return issues;
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Issue getIsssue(@PathVariable("id") int id) {
+    logger.info("Start getIsssue. ID=" + id);       
+    return issueService.getIssue(id);
   }
-   
-  @RequestMapping(value = "/issue/create", method = RequestMethod.POST)
-  public @ResponseBody Issue createIssue(@RequestBody Issue issue) {
-      logger.info("Start createIssue.");
-      issue.setCreatedDate(new Date());
-      issueDatastore.put(issue.getId(), issue);
+  
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  public Issue createIssue(@RequestBody Issue issue) {
+      logger.debug("Adding issue with title {} and description {}", issue.getTitle(), issue.getDescription());
       return issue;
   }
    
-  @RequestMapping(value = "/issue/delete/{id}", method = RequestMethod.PUT)
-  public @ResponseBody Issue deleteIssue(@PathVariable("id") int id) {
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public Issue deleteIssue(@PathVariable("id") int id) {
       logger.info("Start deleteIssue.");
-      Issue issue = issueDatastore.get(id);
-      issueDatastore.remove(id);
-      return issue;
+      return issueService.deleteIssue(id);
   }
 }
--- server/src/main/java/au/com/allianz/codetest/model/Issue.java
+++ server/src/main/java/au/com/allianz/codetest/model/Issue.java
@@ -10,29 +10,55 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 
 public class Issue {
-  
+
   private int id;
-  private String name;
+
+  private String title;
+
+  private String type;
+
+  private String description;
+
   private Date createdDate;
-   
+
   public int getId() {
-      return id;
+    return id;
   }
+
   public void setId(int id) {
-      this.id = id;
+    this.id = id;
   }
-  public String getName() {
-      return name;
+
+  public String getTitle() {
+    return title;
   }
-  public void setName(String name) {
-      this.name = name;
+
+  public void setTitle(String title) {
+    this.title = title;
   }
-   
-  @JsonSerialize(using=DateSerializer.class)
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @JsonSerialize(using = DateSerializer.class)
   public Date getCreatedDate() {
-      return createdDate;
+    return createdDate;
   }
+
   public void setCreatedDate(Date createdDate) {
-      this.createdDate = createdDate;
+    this.createdDate = createdDate;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
   }
 }
